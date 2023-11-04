2f82dbae17ab9da752a54de955199c60224c5abb
Tiago <109687852+tiagomga@users.noreply.github.com>
2023-11-04 01:05:06 +0000

    Add adjustable grid functionality to drawGrid()

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function drawGrid(grid=16*16) {
+function drawGrid(grid=16) {
     const board = document.querySelector('.board');
-    for (let i = 0; i < grid; i++) {
+    const boardSize = board.offsetWidth;
+    const size = boardSize / grid;
+    for (let i = 0; i < grid*grid; i++) {
         const box = document.createElement('div');
         box.classList.toggle('box');
+        box.style.width = `${size}px`;
+        box.style.height = `${size}px`;
         board.appendChild(box);
     }
 }
